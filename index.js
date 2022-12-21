9d8fc37c39981c16d77528e27e86b8c9e99bc893
Palina Mitskevich <palina.mitskevich@gmail.com>
2022-12-21 21:30:36 +0100

    Implements notation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//Init file
+const notation = () => {
+  const value1 = prompt("Введите первое число")
+  if(isNaN(+value1) || value1 === '' || value1 === null) {
+    console.log("Некорректный ввод!")
+    return
+  }
+
+  const value2 = prompt("Введите второе число")
+  if(isNaN(+value2) || value2 === '' || value2 === null || +value2 > 36 || +value2 < 2) {
+    console.log("Некорректный ввод!")
+    return
+  }
+
+  console.log(parseInt(value1, +value2))
+}
+
+notation()
