def780d6d4d38e737d4e6987fac97f1daf588286
Palina Mitskevich <palina.mitskevich@gmail.com>
2022-12-21 21:49:52 +0100

    Implements calculation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//Init file
+const calculation = () => {
+  const value1 = prompt("Введите первое значение")
+  if(isNaN(+value1) || value1 === '' || value1 === null) {
+    console.log("Некорректный ввод!")
+    return
+  }
+
+  const value2 = prompt("Введите второе значение")
+  if(isNaN(+value2) || value2 === '' || value2 === null) {
+    console.log("Некорректный ввод!")
+    return
+  }
+
+  console.log(`Ответ: ${+value1 + +value2}, ${+value1 / +value2}.`)
+}
+
+calculation()
