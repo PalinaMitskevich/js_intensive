5d6ec8bef0f472615e678837b8525bcdf11e2b02
Palina Mitskevich <palina.mitskevich@gmail.com>
2022-12-22 15:19:39 +0100

    Resolves merge conflicts

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const calculation = () => {
-  const value1 = prompt("Введите первое значение")
+const notation = () => {
+  const value1 = prompt("Введите первое число")
   if(isNaN(+value1) || value1 === '' || value1 === null) {
     console.log("Некорректный ввод!")
     return
   }
 
-  const value2 = prompt("Введите второе значение")
-  if(isNaN(+value2) || value2 === '' || value2 === null) {
+  const value2 = prompt("Введите второе число")
+  if(isNaN(+value2) || value2 === '' || value2 === null || +value2 > 36 || +value2 < 2) {
     console.log("Некорректный ввод!")
     return
   }
 
-  console.log(`Ответ: ${+value1 + +value2}, ${+value1 / +value2}.`)
+  console.log(parseInt(value1, +value2))
 }
 
-calculation()
+notation()
